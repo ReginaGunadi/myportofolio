cb3e8a146bb64b71ec665028c759eacf87adb18d
ReginaGunadi <reginagunadi190507@gmail.com>
2026-09-08 16:47:05 +0700

    Implementing MVT in Django (Tutorial 2)

---
+++ main/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Regina Gunadi",
+        "npm": "2506542852",
+        "study_program": "S1 Ilmu Komputer",
+        "bio": (
+            "Undergraduate Computer Science student at Universitas Indonesia. "
+            "Currently exploring data science and machine learning."
+            "A lifelong learner, pianist, math-tech lover and hater."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Regina Gunadi",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
